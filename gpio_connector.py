8586c48900575c3931fc1ed897ebee15d320f75e
Вячеслав <76532024+AtShell@users.noreply.github.com>
2024-05-02 19:00:22 +0500

    Add files via upload

--- gpio_connector.py
+++ gpio_connector.py
@@ -1,15 +1,16 @@
-import OPi.GPIO as GPIO
+# import OPi.GPIO as GPIO
+PIN = 7
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(PIN, GPIO.OUT)
+# GPIO.output(PIN, GPIO.LOW)
 
-PIN=7
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN, GPIO.OUT)
-GPIO.output(PIN, GPIO.LOW)
 
 class GpioConnector():
 
     def LED_on(self):
-        GPIO.output(PIN, GPIO.HIGH)
+        pin = 2
+        # GPIO.output(PIN, GPIO.HIGH)
 
     def LED_off(self):
-        GPIO.output(PIN, GPIO.LOW)
-    
+        pin = 1
+        # GPIO.output(PIN, GPIO.LOW)
--- main.py
+++ main.py
@@ -2,45 +2,46 @@ from fastapi import FastAPI
 import logging
 import gpio_connector
 
-gpio=gpio_connector.GpioConnector()
+gpio = gpio_connector.GpioConnector()
 
 logging.basicConfig(
     level=logging.INFO,
-    filename="py_log.log",filemode="a",
+    filename="py_log.log", filemode="a",
     format="%(message)s %(asctime)s"
-    )
+)
 
-#.venv\Scripts\activate
-#uvicorn main:app --host 0.0.0.0 --port 8000 --reload
-
-logging.info("LED OFF") #base stat on gpio(low)
+logging.info("LED OFF")
 
 app = FastAPI()
+
 
 @app.get('/')
 async def root():
     return {"message": "Hello"}
 
+
 @app.get('/status')
 def status_On():
-    file=open('py_log.log')
-    str=file.readlines()[-1]
-    status=str.find("ON") !=-1
+    file = open('py_log.log')
+    str = file.readlines()[-1]
+    status = str.find("ON") != -1
     file.close()
     return {"status": status}
 
+
 @app.get('/LED_ON')
 def LED_On():
-    if(status_On()!={"status": True}):
+    if (status_On() != {"status": True}):
         gpio.LED_on()
         logging.info("LED ON")
         return {"LED_ON_INFO": "Turned on the LED"}
     else:
         return {"LED_ON_INFO": "LED already on"}
-    
+
+
 @app.get('/LED_OFF')
 def LED_Off():
-    if(status_On()=={"status": True}):
+    if (status_On() == {"status": True}):
         gpio.LED_off()
         logging.info("LED OFF")
         return {"LED_OFF_INFO": "Turned off the LED"}
